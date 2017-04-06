9b65ca4f7edd58dbafc01d9009db0893f9e304f9
Richard Moss <richy8888@hotmail.com>
2017-04-06 21:31:25 +0100

    Further changes to gift section

--- public/index.php
+++ public/index.php
@@ -192,10 +192,12 @@
                 <div class="col-xs-12">
                     <h1>WEDDING GIFTS / PRESENTS</h1>
                     <p class="dklemon">
-                        We do not expect any wedding gifts or presents and as such do not have a list, your presence on our wedding day is more than enough.
+                        We do not expect any wedding gifts or presents and as such do not have a list,<br />
+                        your presence on our wedding day is more than enough.
                     </p>
                     <p class="dklemon">
-                        If you however would like to get us a little something, a small contribution towards one of the following would be greatly appreciated:
+                        If you however would like to get us a little something, a small contribution towards<br />
+                        one of the following would be greatly appreciated:
                     </p>
                     <ul class="dklemon">
                       <li>A new dining room table and chairs</li>
